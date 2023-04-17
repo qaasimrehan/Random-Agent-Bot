b00b2bad4ec01f59dc6a57ff783fb7e4f6ba3d9b
Qaasim Rehan <qaasimrehan@gmail.com>
2023-04-17 16:21:16 -0400

    Added RandomAgents(num)  to print multiple agents

--- RandomAgentselect.py
+++ RandomAgentselect.py
@@ -14,7 +14,8 @@ with open('token.txt', 'r') as file:
     token = file.read().rstrip()
 
 Agents = ["Jett","Killjoy", "Sova", "Omen", "Gekko", "KAY/O", "Skye", "Viper", "Fade", "Cypher", "Phoenix", "Yoru", "Neon", "Raze", "Reyna", "Astra", "Harbor" ]
-agentImage =["Jett.png", "Killjoy.png", "Sova.png", "Omen.png", "Gekko.png", "KAYO.png", "Skye.png", "Viper.png", "Fade.png", "Cypher.png", " Phoenix.png", "Yoru.png", "Neon.png", "Raze.png", "Reyna.png", "Astra.png", "Harbor.png"]
+agentImage = ["Jett.png", "Killjoy.png", "Sova.png", "Omen.png", "Gekko.png", "KAYO.png", "Skye.png", "Viper.png", "Fade.png", "Cypher.png", "Phoenix.png", "Yoru.png", "Neon.png", "Raze.png", "Reyna.png", "Astra.png", "Harbor.png"]
+SideAffect = [""]
 @bot.event
 async def on_ready():
     print("Agent select is ready")
@@ -27,5 +28,18 @@ async def RandomAgent(ctx):
         embed.set_image(url="attachment://image.png")
         await ctx.send(file=file,embed = embed)
          
+@bot.command(pass_context = True)
+async def RandomAgents(ctx, num):
+        tempAgents=Agents.copy()
+        tempagentImages=agentImage.copy()
+        for y in range(int(num)):
+            x = random.randint(0, len(tempAgents)-1)
+            embed = discord.Embed(title = "Random Agent " + str(y+1), description = (tempAgents[x]), color = (0xF85252))
+            file = discord.File("Images/"+ (tempagentImages[x]), filename="image.png")
+            embed.set_image(url="attachment://image.png")
+            tempAgents.pop(x)
+            tempagentImages.pop(x)
+            await ctx.send(file=file,embed = embed)
+        
 
 bot.run(token)
